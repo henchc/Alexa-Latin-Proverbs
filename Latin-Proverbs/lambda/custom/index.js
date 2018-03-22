f64793ee2d91ce2a57ff2358bb39d3c355165f21
Christopher Hench <chris.l.hench@gmail.com>
2018-03-21 18:50:29 -0700

    model tweaks

--- Latin-Proverbs/lambda/custom/index.js
+++ Latin-Proverbs/lambda/custom/index.js
@@ -1,9 +1,9 @@
 'use strict';
+
+// load packages
 var Alexa = require("alexa-sdk");
-
-// For detailed tutorial on how to making a Alexa skill,
-// please visit us at http://alexa.design/build
-
+var https = require("https");
+var cheerio = require("cheerio");
 
 exports.handler = function(event, context) {
     var alexa = Alexa.handler(event, context);
@@ -12,44 +12,128 @@ exports.handler = function(event, context) {
 };
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello');
-    },
-    'MyNameIsIntent': function () {
-        this.emit('SayHelloName');
-    },
-    'SayHello': function () {
-        this.response.speak('Hello World!')
-                     .cardRenderer('hello world', 'hello world');
-        this.emit(':responseReady');
-    },
-    'SayHelloName': function () {
-        var name = this.event.request.intent.slots.name.value;
-        this.response.speak('Hello ' + name)
-            .cardRenderer('hello world', 'hello ' + name);
-        this.emit(':responseReady');
-    },
-    'SessionEndedRequest' : function() {
-        console.log('Session ended with reason: ' + this.event.request.reason);
+    'LatinProverbsIntent': function () {
+        httpsGet((alexaSpeak, alexaCard) => {
+                console.log("sent     : request");
+                console.log("received to speak : " + alexaSpeak);
+                console.log("received for card : " + alexaCard)
+
+                this.response.speak(alexaSpeak)
+                             .cardRenderer(alexaCard);
+                this.emit(':responseReady');
+            }
+        );
     },
     'AMAZON.StopIntent' : function() {
         this.response.speak('Bye');
         this.emit(':responseReady');
     },
     'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
-            " name is awesome Aaron'");
+        this.response.speak("You can try: 'alexa, tell me a latin proverb'");
         this.emit(':responseReady');
     },
     'AMAZON.CancelIntent' : function() {
         this.response.speak('Bye');
         this.emit(':responseReady');
-    },
-    'Unhandled' : function() {
-        this.response.speak("Sorry, I didn't get that. You can try: 'alexa, hello world'" +
-            " or 'alexa, ask hello world my name is awesome Aaron'");
     }
 };
+
+function httpsGet(callback) {
+
+    // url for wikiquote Latin proverbs
+    var url = 'https://en.wikiquote.org/wiki/Latin_proverbs';
+
+    // make request
+    var req = https.request(url, res => {
+        res.setEncoding('utf8');
+        var returnData = "";
+
+        // save response to returnData
+        res.on('data', chunk => {
+            returnData = returnData + chunk;
+        });
+
+        res.on('end', () => {
+
+            // load page source to cheerio parser
+            const $ = cheerio.load(returnData);
+
+            // initiate proverb in case we get empty element
+            var proverb = '';
+
+            // until we get fillied italic element in the first li of a ul
+            while (proverb.trim().length == 0) {
+
+                // random index for all uls
+                var randomIndex = Math.floor(Math.random()*$('ul').length);
+
+                // assign proverb and translation
+                proverb = $('ul').eq(randomIndex).find('li').first().find('i').first().text()
+                var translation = $('ul').eq(randomIndex).find('li').eq(1).text()
+            }
+
+            // take out whatever comes before colon and the colon and space
+            translation = translation.substring(translation.indexOf(':') + 1).trim()
+
+            // trans proverb to IPA + SSML
+            var latinIPA = transIPA(proverb.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""))
+
+            // build speak and card text
+            var alexaSpeak = latinIPA + ' ' + translation
+            var alexaCard = "Proverb: " + proverb + '\n' + "English: " + translation
+
+            callback(alexaSpeak, alexaCard)
+        });
+    });
+    req.end();
+}
+
+function transIPA(latin) {
+
+    // list of phonemes to translate
+    // NOTE order matters in replace operation
+    // some simplifications are made
+    var phonemeList = [['ch', 'kʰ'],
+                       ['qu', 'kʷ'],
+                       ['ng', 'ŋ'],
+                       ['ph', 'pʰ'],
+                       ['th', 'tʰ'],
+                       ['ae', 'ae̯'],
+                       ['oe', 'oe̯'],
+                       ['au', 'au̯'],
+                       ['eu', 'eu̯'],
+                       ['ui', 'ui̯'],
+                       ['b', 'b'],
+                       ['d', 'd'],
+                       ['f', 'f'],
+                       ['g', 'g'],
+                       ['h', 'h'],
+                       ['i', 'ɪ'],
+                       ['c', 'k'],
+                       ['k', 'k'],
+                       ['l', 'l'],
+                       ['m', 'm'],
+                       ['n', 'n'],
+                       ['p', 'p'],
+                       ['r', 'r'],
+                       ['s', 's'],
+                       ['t', 't'],
+                       ['z', 'z'],
+                       ['a', 'aː'],
+                       ['e', 'ɛ'],
+                       ['o', 'oː'],
+                       ['u', 'uː'],
+                       ['y', 'ʏ']];
+
+    // repalce each pair in order
+    var phonemeLatin = latin;
+    for (var i = 0; i < phonemeList.length; i++) {
+        var regex = new RegExp(phonemeList[i][0], 'g');
+        phonemeLatin = phonemeLatin.replace(regex, phonemeList[i][1]);
+    }
+
+    // build SSML
+    var phonemeSSML = '<phoneme alphabet="ipa" ph="' + phonemeLatin + '" />' + "<break time='1s'/>";
+
+    return phonemeSSML
+}
--- testing.js
+++ testing.js
@@ -4,23 +4,16 @@
 var https = require("https");
 var cheerio = require("cheerio");
 
-// set first page of BTWB wods
-var page = 1;
-
-var myRequest = "2018-01-14"
-
-httpsGet(myRequest,  page, (myResult) => {
-        console.log("sent     : " + myRequest);
-        console.log("received : " + myResult);
+httpsGet((alexaSpeak, alexaCard) => {
+        console.log("sent     : request");
+        console.log("received to speak : " + alexaSpeak);
+        console.log("received for card : " + alexaCard)
     }
 );
 
+function httpsGet(callback) {
 
-////////
-// make GET req to BTWB
-function httpsGet(userRequest, page, callback) {
-
-    // build URL with page num
+    // url for wikiquote Latin proverbs
     var url = 'https://en.wikiquote.org/wiki/Latin_proverbs';
 
     // make request
@@ -38,17 +31,29 @@ function httpsGet(userRequest, page, callback) {
             // load page source to cheerio parser
             const $ = cheerio.load(returnData);
 
+            // initiate proverb in case we get empty element
             var proverb = '';
+
+            // until we get fillied italic element in the first li of a ul
             while (proverb.trim().length == 0) {
+
+                // random index for all uls
                 var randomIndex = Math.floor(Math.random()*$('ul').length);
-                var proverb = $('ul').eq(randomIndex).find('li').first().find('i').first().text()
+
+                // assign proverb and translation
+                proverb = $('ul').eq(randomIndex).find('li').first().find('i').first().text()
                 var translation = $('ul').eq(randomIndex).find('li').eq(1).text()
             }
 
+            // take out whatever comes before colon and the colon and space
             translation = translation.substring(translation.indexOf(':') + 1).trim()
-            var latinIPA = transIPA(proverb)
+
+            // trans proverb to IPA + SSML
+            var latinIPA = transIPA(proverb.toLowerCase())
+
+            // build speak and card text
             var alexaSpeak = latinIPA + ' ' + translation
-            var alexaCard = proverb + '\n' + translation
+            var alexaCard = "Proverb: " + proverb + '\n' + "English: " + translation
 
             callback(alexaSpeak, alexaCard)
         });
@@ -57,44 +62,50 @@ function httpsGet(userRequest, page, callback) {
 }
 
 function transIPA(latin) {
-    var phonemeList = [['ch', 'kʰ'],
-                     ['qu', 'kʷ'],
-                     ['ng', 'ŋ'],
-                     ['ph', 'pʰ'],
-                     ['th', 'tʰ'],
-                     ['ae', 'ae̯'],
-                     ['oe', 'oe̯'],
-                     ['au', 'au̯'],
-                     ['eu', 'eu̯'],
-                     ['ui', 'ui̯'],
-                     ['b', 'b'],
-                     ['d', 'd'],
-                     ['f', 'f'],
-                     ['g', 'g'],
-                     ['h', 'h'],
-                     ['i', 'ɪ'],
-                     ['c', 'k'],
-                     ['k', 'k'],
-                     ['l', 'l'],
-                     ['m', 'm'],
-                     ['n', 'n'],
-                     ['p', 'p'],
-                     ['r', 'r'],
-                     ['s', 's'],
-                     ['t', 't'],
-                     ['z', 'z'],
-                     ['a', 'aː'],
-                     ['e', 'ɛ'],
-                     ['o', 'oː'],
-                     ['u', 'uː'],
-                     ['y', 'ʏ']];
 
+    // list of phonemes to translate
+    // NOTE order matters in replace operation
+    // some simplifications are made
+    var phonemeList = [['ch', 'kʰ'],
+                       ['qu', 'kʷ'],
+                       ['ng', 'ŋ'],
+                       ['ph', 'pʰ'],
+                       ['th', 'tʰ'],
+                       ['ae', 'ae̯'],
+                       ['oe', 'oe̯'],
+                       ['au', 'au̯'],
+                       ['eu', 'eu̯'],
+                       ['ui', 'ui̯'],
+                       ['b', 'b'],
+                       ['d', 'd'],
+                       ['f', 'f'],
+                       ['g', 'g'],
+                       ['h', 'h'],
+                       ['i', 'ɪ'],
+                       ['c', 'k'],
+                       ['k', 'k'],
+                       ['l', 'l'],
+                       ['m', 'm'],
+                       ['n', 'n'],
+                       ['p', 'p'],
+                       ['r', 'r'],
+                       ['s', 's'],
+                       ['t', 't'],
+                       ['z', 'z'],
+                       ['a', 'aː'],
+                       ['e', 'ɛ'],
+                       ['o', 'oː'],
+                       ['u', 'uː'],
+                       ['y', 'ʏ']];
+
+    // repalce each pair in order
     var phonemeLatin = latin;
     for (var i = 0; i < phonemeList.length; i++) {
         var regex = new RegExp(phonemeList[i][0], 'g');
         phonemeLatin = phonemeLatin.replace(regex, phonemeList[i][1]);
     }
 
+    // build SSML
     var phonemeSSML = '<phoneme alphabet="ipa" ph="' + phonemeLatin + '" />' + "<break time='1s'/>";
 
     return phonemeSSML
